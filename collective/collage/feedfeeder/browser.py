4b2de25a14eea558fbdb7bd4044f06c561c57ef7
Maurits van Rees <maurits@vanrees.org>
2012-10-13 22:05:47 +0200

    Nuke trailing white space.

--- collective/collage/feedfeeder/browser.py
+++ collective/collage/feedfeeder/browser.py
@@ -5,11 +5,11 @@ class FeedFolderView(FeedFolderView):
     title = u"Standard"
 
     batch_size = 20
-    
+
     @property
     def batch(self):
         results = self.item_list()
         b_size = self.request.get('b_size', self.batch_size)
-        b_start = self.request.get('b_start', 0)        
+        b_start = self.request.get('b_start', 0)
         return Batch(results, b_size, int(b_start), orphan=1)
-                    
+
